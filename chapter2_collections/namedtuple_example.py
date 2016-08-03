92773284aaeef833f7fb190f7bb9a740641754ec
Mike Driscoll <mdriscoll@agleader.com>
2016-08-03 13:48:18 -0500

    fixed namedtuple example

--- chapter2_collections/namedtuple_example.py
+++ chapter2_collections/namedtuple_example.py
@@ -3,4 +3,4 @@ from collections import namedtuple
 Parts = namedtuple('Parts', 'id_num desc cost amount')
 auto_parts = Parts(id_num='1234', desc='Ford Engine',
                    cost=1200.00, amount=10)
-print auto_parts.id_num
+print (auto_parts.id_num)
