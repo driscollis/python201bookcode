a49a1c8bfe48a4742b7a05bd1ea132a03cc1025b
Mike Driscoll <mike@pythonlibrary.org>
2017-03-08 21:15:32 -0600

    fix some errors in the example

--- chapter27_asyncio/tasks.py
+++ chapter27_asyncio/tasks.py
@@ -1,16 +1,16 @@
 import asyncio
-import time
-
+ 
+ 
 async def my_task(seconds):
     """
     A task to do for a number of seconds
     """
     print('This task is taking {} seconds to complete'.format(
         seconds))
-    time.sleep(seconds)
+    await asyncio.sleep(seconds)
     return 'task finished'
-
-
+ 
+ 
 if __name__ == '__main__':
     my_event_loop = asyncio.get_event_loop()
     try:
@@ -19,5 +19,5 @@ if __name__ == '__main__':
         my_event_loop.run_until_complete(task_obj)
     finally:
         my_event_loop.close()
-
+ 
     print("The task's result was: {}".format(task_obj.result()))
