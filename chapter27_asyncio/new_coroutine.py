f5206747df971a934dbc4ded8fabc9fa9787506b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-14 10:13:35 +0800

    Delete chapter27_asyncio directory

--- chapter27_asyncio/new_coroutine.py
+++
@@ -1,6 +0,0 @@
-# Python 3.5 coroutine example
-
-import asyncio
-
-async def my_coro():
-    await func()
--- chapter27_asyncio/old_coroutine.py
+++
@@ -1,6 +0,0 @@
-# Python 3.4 coroutine example
-import asyncio
-
-@asyncio.coroutine
-def my_coro():
-    yield from func()
