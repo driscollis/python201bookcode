307fba015c93adbcf918b13c958abe72bd28817a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-14 09:46:18 +0800

    Delete chapter22_urllib directory

--- chapter22_urllib/robot_parser_example.py
+++
@@ -1,8 +0,0 @@
-import urllib.robotparser
-
-
-robot = urllib.robotparser.RobotFileParser()
-robot.set_url('http://arstechnica.com/robots.txt')
-print(robot.read())
-print(robot.can_fetch('*', 'http://arstechnica.com/'))
-print(robot.can_fetch('*', 'http://arstechnica.com/cgi-bin/'))
