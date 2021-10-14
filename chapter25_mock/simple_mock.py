a8bc84d39dcaff0533fee9f7e1f33f6058eab863
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-14 10:11:10 +0800

    Delete chapter25_mock directory

--- chapter25_mock/simple_mock.py
+++
@@ -1,6 +0,0 @@
-from unittest.mock import Mock
-
-
-my_mock = Mock()
-my_mock.__str__ = Mock(return_value='Mocking')
-print(str(my_mock))
--- chapter25_mock/webreader.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-
-def read_webpage(url):
-    response = urllib.request.urlopen(url)
-    return response.read()
